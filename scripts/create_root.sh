5b9001b13b1f23d4aa63b38eab4b4c2c0d4e725d
arlotito <arturo.lotito@microsoft.com>
2021-08-28 16:39:59 +0200

    wip

--- scripts/create_root.sh
+++ scripts/create_root.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
+# usage: ./create_root.sh <root_name>
+#
+# example:
+#   ./create_root.sh myCA  
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-mkdir -p /root/ca
-cp root.openssl.cnf /root/ca/openssl.cnf 
+ROOT_CA=/root/$1
 
-cd /root/ca
-
-mkdir -p certs crl newcerts private
-
-chmod 700 private
-touch index.txt
-echo 1000 > serial
+mkdir -p ${ROOT_CA}/certs ${ROOT_CA}/crl ${ROOT_CA}/newcerts ${ROOT_CA}/private
 
 # create root key
-cd /root/ca
-openssl genrsa -aes256 -out private/ca.key.pem 4096
-chmod 400 private/ca.key.pem
+openssl genrsa -aes256 -out ${ROOT_CA}/private/ca.key.pem 4096
+chmod 400 ${ROOT_CA}/private/ca.key.pem
 
 # create root cert
-cd /root/ca
-
 openssl req -config openssl.cnf \
-    -key private/ca.key.pem \
+    -key ${ROOT_CA}/private/ca.key.pem \
     -new -x509 -days 7300 -sha256 -extensions v3_ca \
-    -out certs/ca.cert.pem
-chmod 444 certs/ca.cert.pem
+    -out ${ROOT_CA}/certs/ca.cert.pem
+chmod 444 ${ROOT_CA}/certs/ca.cert.pem
